48fd9f8ed830d053e6a86f3c7010d7ad62f19d12
Bini Alex <61168877+alemayehubini@users.noreply.github.com>
2020-12-15 08:50:32 -0500

    first push

---
+++ BiniTutor/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BiniTutor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //[Authorize] //Accessed by all registered users
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        //[Authorize(Roles ="Superuser")] //Accessed by Super users
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ BiniTutor/Controllers/RoleController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BiniTutor.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BiniTutor.Controllers
+{
+    public class RoleController : Controller
+    {
+        ApplicationDbContext context;
+        
+        public RoleController()
+        {
+            context = new ApplicationDbContext();
+        }
+        
+        // GET: Role
+        public ActionResult Index()
+        {
+            var Roles = context.Roles.ToList();
+            return View(Roles);
+        }
+
+        
+        public ActionResult Create()
+        {
+            var Role = new IdentityRole();
+            return View(Role);
+        }
+
+        [HttpPost]
+        public ActionResult Create(IdentityRole Role)
+        {
+            context.Roles.Add(Role);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
